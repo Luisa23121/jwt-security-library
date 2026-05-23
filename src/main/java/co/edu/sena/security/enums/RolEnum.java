61c3d4ef223f8353c5620382c080c7bcb2b82edc
Luisa23121 <luisamaria23121@gmail.com>
2026-05-23 00:37:10 -0500

    prueba de annotacion general

---
+++ src/main/java/co/edu/sena/security/enums/RolEnum.java
@@ -0,0 +1,16 @@
+package co.edu.sena.security.enums;
+
+
+// librería
+public enum RolEnum {
+        ADMINISTRADOR,
+        INSTRUCTOR,
+        CHEF,
+        MESERO,
+        BARTENDER,
+        CAJERO,
+        CONTADORA,
+        AUXILIAR_COCINA,
+        COMENSAL
+    }
+
